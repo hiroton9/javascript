ab25fc0764571763df7892d698786328584695dc
knishita@centos6.localhost <hiroton9@gmail.com>
2015-05-26 16:16:50 +0900

    変数の扱い

--- js2.js
+++ js2.js
@@ -1,6 +1,8 @@
 function printMsg() {
         alert("Hello!");
 }
-function changeMsg() {
-    document.myform1.mytext1.value = "変更しました";
+function changeMsg( _count ) {
+    _count++;
+    document.myform1.mytext1.value = _count + "回 変更しました";
+    return _count;
 }
