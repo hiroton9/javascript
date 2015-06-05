d09346e72bfdcbf162d95ceae02e860a768a75b4
hiroton9 <hiroton9@gmail.com>
2015-06-05 11:49:27 +0900

    test

--- test.sql
+++ test.sql
@@ -25,4 +25,27 @@ WHERE EF.wid = '400' AND EF.state = 'X' AND
    ) 
 )order by sid
 
-
+--wid 100 で A かつ、200でA　、かつ 500でA
+-- もしくは
+--wid 200 で A かつ、600はない、かつ 500でAでない
+-- 以上の中で
+--wid 300 で A かつ、800はない
+SELECT AB.sid
+FROM   pana AB
+WHERE  AB.wid = '300' and AB.state = 'A' 
+AND    NOT EXISTS ( SELECT d.sid from pana d where AB.sid = d.sid and d.wid = '800' )
+AND   (
+      EXISTS (
+        select a.sid from pana a where  a.wid = '100' and a.state = 'A'
+        and    EXISTS ( select b.sid from pana b where b.sid = a.sid and b.wid = '200' and b.state = 'A' )
+        and    EXISTS ( select c.sid from pana c where c.sid = a.sid and c.wid = '500' and c.state = 'A' )
+        and AB.sid = a.sid
+      )
+      OR
+      EXISTS (
+        select a.sid from pana a where  a.wid = '200' and a.state = 'A'
+        and    NOT EXISTS ( select b.sid from pana b where b.sid = a.sid and b.wid = '600' )
+        and    EXISTS ( select c.sid from pana c where c.sid = a.sid and c.wid = '500' and c.state != 'A' )
+        and AB.sid = a.sid
+      )
+) 
